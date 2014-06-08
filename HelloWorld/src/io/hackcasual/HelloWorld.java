9454e2bde0cb4ae149d8a810f4a16e1ccc2790ab
Charles Vaughn <cvaughn@gmail.com>
2014-06-08 13:52:29 -0700

    Cleaned up helloworld example

--- HelloWorld/src/io/hackcasual/HelloWorld.java
+++ HelloWorld/src/io/hackcasual/HelloWorld.java
@@ -5,8 +5,7 @@ import javacard.security.CryptoException;
 import javacard.security.MessageDigest;
 
 public class HelloWorld extends Applet {
-	private final static byte[] hello = { 0x48, 0x65, 0x6c, 0x6c, 0x6f } ;
-	private final static byte[] empty = { 0x00, 0x00, 0x00, 0x00, 0x00 } ;
+	private final static byte[] hello = { 'H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd', '!' } ;
 	
 	public void process(APDU apdu) throws ISOException {
 		byte[] buf = apdu.getBuffer();
@@ -15,29 +14,17 @@ public class HelloWorld extends Applet {
 		
 		
 		switch(buf[ISO7816.OFFSET_INS]) {
+		// Called on AID select
 		case (byte)0xA4:
-			Util.arrayCopy(hello, (short)0, buf, ISO7816.OFFSET_CDATA, (short)5);
-			apdu.setOutgoingAndSend(ISO7816.OFFSET_CDATA, (byte)5);
+			break;
+		// Our function
+		case (byte)0x01:
+			MessageDigest md = MessageDigest.getInstance(MessageDigest.ALG_SHA_256, false);
+			md.doFinal(hello, (short)0, (short)hello.length, buf, ISO7816.OFFSET_CDATA);
+			apdu.setOutgoingAndSend(ISO7816.OFFSET_CDATA, (byte)MessageDigest.LENGTH_SHA_256);
 			break;
 		default:
-			Util.arrayCopy(empty, (short)0, buf, ISO7816.OFFSET_CDATA, (short)5);
-			buf[ISO7816.OFFSET_CDATA] = buf[ISO7816.OFFSET_INS];
-			try {
-				MessageDigest md = MessageDigest.getInstance(MessageDigest.ALG_SHA_256, false);
-				md.doFinal(hello, (short)0, (short)5, buf, ISO7816.OFFSET_CDATA);
-			} catch (RuntimeException cx) {
-				//buf[ISO7816.OFFSET_CDATA + 1] = (byte)(cx.getReason() >> 8);
-				//buf[ISO7816.OFFSET_CDATA + 2] = (byte)(cx.getReason());
-				buf[ISO7816.OFFSET_CDATA + 1] = (byte)0x56;
-				if (cx instanceof NullPointerException) {
-					buf[ISO7816.OFFSET_CDATA + 2] = (byte)0x78;
-				}
-				if (cx instanceof CryptoException) {
-					buf[ISO7816.OFFSET_CDATA + 2] = (byte)0x90;
-				}				
-			}
-			apdu.setOutgoingAndSend(ISO7816.OFFSET_CDATA, (byte)5);
-			break;//ISOException.throwIt(ISO7816.SW_INS_NOT_SUPPORTED);
+			ISOException.throwIt(ISO7816.SW_INS_NOT_SUPPORTED);
 		}
 
 	}
